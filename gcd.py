3fc13a9e02147ebde953d186544c1af85a85eecb
IAM78 <91953698+IAM78@users.noreply.github.com>
2021-10-19 11:08:00 -0700

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,22 @@
+a = int(input(" the first number: "))
+b = int(input(" the seccond number: "))
+
+
+
+gcd = 1
+if a > b:
+    temp = b
+
+else:
+
+    temp = a
+for i in range(temp, 1, -1):
+    if ((a % i == 0) and (b % i == 0)):
+        gcd = i
+        
+        break
+
+
+
+
+print(f"GCD({a},{b}) is: {gcd}")
